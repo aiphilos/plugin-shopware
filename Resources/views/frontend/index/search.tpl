ff6b87c895f3493aac90dfbe5fbc8ec2c635c749
Stephan Lanfermann <sl@designverign.de>
2018-04-03 15:45:24 +0200

    Removed character limit in frontend search box

---
+++ Resources/views/frontend/index/search.tpl
@@ -0,0 +1,5 @@
+{extends file="parent:frontend/index/search.tpl"}
+
+{block name='frontend_index_search_field'}
+    <input type="search" name="sSearch" class="main-search--field" autocomplete="off" autocapitalize="off" placeholder="{s name="IndexSearchFieldPlaceholder"}{/s}" maxlength="6000"  />
+{/block}
---
+++ Subscriber/TemplateRegistration.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sl
+ * Date: 03.04.18
+ * Time: 15:34
+ */
+
+namespace VerignAiPhilosSearch\Subscriber;
+
+
+use Enlight\Event\SubscriberInterface;
+
+class TemplateRegistration implements SubscriberInterface
+{
+    /** @var \Enlight_Template_Manager */
+    private $templateManager;
+
+    /**
+     * TemplateRegistration constructor.
+     * @param \Enlight_Template_Manager $templateManager
+     */
+    public function __construct(\Enlight_Template_Manager $templateManager) {
+        $this->templateManager = $templateManager;
+    }
+
+
+    public static function getSubscribedEvents() {
+        return [
+            'Enlight_Controller_Action_PostDispatchSecure_Frontend' => 'extendTemplateDir'
+        ];
+    }
+
+    public function extendTemplateDir(\Enlight_Event_EventArgs $args) {
+        $this->templateManager->addTemplateDir(__DIR__ . '/../Resources/views');
+    }
+}
